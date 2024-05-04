297a68f6226e3089db2cc1792ce648e50fe173f1
javese <3583758@qq.com>
2024-05-04 15:18:08 +0800

    Update gestureScroll.py
    use cvzone package.

--- gestureScroll.py
+++ gestureScroll.py
@@ -1,163 +1,71 @@
-#coding=utf-8
+from cvzone.HandTrackingModule import HandDetector
 import cv2
 import pyautogui
-import numpy as np
-import time
-import math
-import mediapipe as mp
-class handDetector():
-    def __init__(self, mode=False, maxHands=2, model_complexity=1, detectionCon=0.8, trackCon=0.8):
-        self.mode = mode
-        self.maxHands = maxHands
-        self.detectionCon = detectionCon
-        self.trackCon = trackCon
-        self.model_complexity = model_complexity
-
-        self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.model_complexity,self.detectionCon, self.trackCon)
-        self.mpDraw = mp.solutions.drawing_utils
-        self.tipIds = [4, 8, 12, 16, 20]
-
-    def findHands(self, img, draw=True):
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        self.results = self.hands.process(imgRGB)
-
-        #print(self.results.multi_handedness)  # 获取检测结果中的左右手标签并打印
-
-        if self.results.multi_hand_landmarks:
-            for handLms in self.results.multi_hand_landmarks:
-                if draw:
-                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
-        return img
-
-    def findPosition(self, img, draw=True):
-        self.lmList = []
-        if self.results.multi_hand_landmarks:
-            for handLms in self.results.multi_hand_landmarks:
-                for id, lm in enumerate(handLms.landmark):
-                    h, w, c = img.shape
-                    cx, cy = int(lm.x * w), int(lm.y * h)
-                    # print(id, cx, cy)
-                    self.lmList.append([id, cx, cy])
-                    if draw:
-                        cv2.circle(img, (cx, cy), 12, (255, 0, 255), cv2.FILLED)
-        return self.lmList
-
-    def fingersUp(self):
-        fingers = []
-        # 大拇指
-        if (self.lmList[self.tipIds[0]][2] +10 < self.lmList[self.tipIds[0] - 2][2]) and (self.lmList[self.tipIds[0]][2] < self.lmList[self.tipIds[1]][2]):
-            fingers.append(1)
-        else:
-            fingers.append(0)
-
-        # 其余手指
-        for id in range(1, 5):
-            if (self.lmList[self.tipIds[id]][2] + 20 < self.lmList[self.tipIds[id] - 2][2]):
-                fingers.append(1)
-            else:
-                fingers.append(0)
-
-        # totalFingers = fingers.count(1)
-        return fingers
-
-    def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
-        x1, y1 = self.lmList[p1][1:]
-        x2, y2 = self.lmList[p2][1:]
-        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
-
-        if draw:
-            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
-            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
-            length = math.hypot(x2 - x1, y2 - y1)
-
-        return length, img, [x1, y1, x2, y2, cx, cy]
 
 
-def main():
-    pTime = 0
-    cTime = 0
-    cap = cv2.VideoCapture(0)
-    detector = handDetector()
-    while True:
-        success, img = cap.read()
-        img = detector.findHands(img)        # 检测手势并画上骨架信息
+# Initialize the webcam to capture video
+# The '2' indicates the third camera connected to your computer; '0' would usually refer to the built-in camera
+cap = cv2.VideoCapture(0)
 
-        lmList = detector.findPosition(img)  # 获取得到坐标点的列表
+# Initialize the HandDetector class with the given parameters
+detector = HandDetector(staticMode=False, maxHands=2, modelComplexity=1, detectionCon=0.5, minTrackCon=0.5)
 
-
-
-
-        # k = cv2.waitKey(1) & 0xFF  # 判断按键
-
-
-
-        if len(lmList) != 0:
-            print(lmList[4])
-
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-
-        cv2.putText(img, 'fps:' + str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-        cv2.imshow('Image', img)
-        cv2.waitKey(1)
-
-##############################
-wCam, hCam = 1000, 1000
-frameR = 100
-smoothening = 5
-##############################
-cap = cv2.VideoCapture(0)  # 若使用笔记本自带摄像头则编号为0  若使用外接摄像头 则更改为1或其他编号
-#设置摄像头的呈现画面的宽高
-cap.set(3, wCam)
-cap.set(4, hCam)
-pTime = 0
-plocX, plocY = 0, 0
-clocX, clocY = 0, 0
-
-detector = handDetector()
-wScr, hScr = pyautogui.size()
-# print(wScr, hScr)
-
+# Continuously get frames from the webcam
 while True:
+    # Capture each frame from the webcam
+    # 'success' will be True if the frame is successfully captured, 'img' will contain the frame
     success, img = cap.read()
-    # 1. 检测手部 得到手指关键点坐标
-    img = detector.findHands(img)
-    #cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR), (0, 255, 0), 2,  cv2.FONT_HERSHEY_PLAIN)
-    lmList = detector.findPosition(img, draw=False)
 
+    # Find hands in the current frame
+    # The 'draw' parameter draws landmarks and hand outlines on the image if set to True
+    # The 'flipType' parameter flips the image, making it easier for some detections
+    hands, img = detector.findHands(img, draw=True, flipType=True)
 
+    # Check if any hands are detected
+    if hands:
+        # Information for the first hand detected
+        hand1 = hands[0]  # Get the first hand detected
+        lmList1 = hand1["lmList"]  # List of 21 landmarks for the first hand
+        bbox1 = hand1["bbox"]  # Bounding box around the first hand (x,y,w,h coordinates)
+        center1 = hand1['center']  # Center coordinates of the first hand
+        handType1 = hand1["type"]  # Type of the first hand ("Left" or "Right")
 
-
-    # k = cv2.waitKey() & 0xFF  # 判断按键
-
-    # 2. 判断食指和中指是否伸出
-    if len(lmList) != 0:
-        x1, y1 = lmList[8][1:]
-        x2, y2 = lmList[12][1:]
-        fingers = detector.fingersUp()
-        print(f'{lmList[4][2]},{lmList[2][2]},{lmList[6][2]},{fingers[0]},{fingers[1]},{fingers[2]},{fingers[3]},{fingers[4]}',end='\r')
-        #print(lmList+fingers,end='\r')
-        if fingers[0]:#伸拇指往下翻
+        # Count the number of fingers up for the first hand
+        fingers1 = detector.fingersUp(hand1)
+        #print(f'H1 = {fingers1}', end="\r")  # Print the count of fingers that are up
+        if fingers1[0]:#伸拇指往下翻
             pyautogui.scroll(-200);
         else:
-            if fingers[1]:#伸食指往上翻
+            if fingers1[1]:#伸食指往上翻
                 pyautogui.scroll(200);
-        if fingers[1] and fingers[2] and fingers[3] and fingers[4]:
-            print("\ngesture exit")
+
+        # Check if a second hand is detected
+        if len(hands) == 2:
+            # Information for the second hand
+            hand2 = hands[1]
+            lmList2 = hand2["lmList"]
+            bbox2 = hand2["bbox"]
+            center2 = hand2['center']
+            handType2 = hand2["type"]
+
+            # Count the number of fingers up for the second hand
+            fingers2 = detector.fingersUp(hand2)
+            if fingers2[0]:#伸拇指往下翻
+                pyautogui.scroll(-200);
+            else:
+                if fingers2[1]:#伸食指往上翻
+                    pyautogui.scroll(200);            
+            if fingers1[0] and fingers1[1] and fingers1[2] and fingers1[3] and fingers1[4] and \
+                fingers2[0] and fingers2[1] and fingers2[2] and fingers2[3] and fingers2[4] :
+                print("gesture exit")
+                break;
+
+    # Display the image in a window
+    #cv2.imshow("Image", img)
+
+    # Keep the window open and update it for each frame; wait for 1 millisecond between frames
+    key = cv2.waitKey(50) & 0xFF
+    if key == ord('q'):
             break
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-    #cv2.putText(img, f'fps:{int(fps)}', (15, 25),cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
-    #cv2.imshow("Gesture Scroll", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        print("\nkey exit")
-        break
-#释放摄像头
 cap.release()
-#释放内存
 cv2.destroyAllWindows()
